e7714c4c8072b2b41d5ba11edea1f10f9730a35a
glen coden <info@simonmeyer.de>
2024-09-29 23:26:11 +0200

    add tailwind-config package

--- apps/cloud/tailwind.config.ts
+++ apps/cloud/tailwind.config.ts
@@ -1,22 +1,7 @@
 import type { Config } from 'tailwindcss'
+import baseConfig from '@repo/tailwind-config/base'
 
 export default {
     content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: [
-                    '"Inter"',
-                    'ui-sans-serif',
-                    'system-ui',
-                    'sans-serif',
-                    '"Apple Color Emoji"',
-                    '"Segoe UI Emoji"',
-                    '"Segoe UI Symbol"',
-                    '"Noto Color Emoji"',
-                ],
-            },
-        },
-    },
-    plugins: [],
+    presets: [baseConfig],
 } satisfies Config
---
+++ packages/tailwind-config/base.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    '"Inter"',
+                    'ui-sans-serif',
+                    'system-ui',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
+        },
+    },
+    plugins: [],
+} satisfies Omit<Config, 'content'>

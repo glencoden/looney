4cad405f2ecd0ff8ed3e541a4486dc79337d2b49
Glen Coden <info@simonmeyer.de>
2025-05-17 11:57:37 +0200

    chore: assign specific dev ports to apps

--- apps/tool/vite.config.ts
+++ apps/tool/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+    server: {
+        port: 3003,
+    },
     plugins: [
         remix({
             future: {

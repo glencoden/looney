cd2bcad604cd12cced6e6119c56643abf8115e34
Glen Coden <info@simonmeyer.de>
2025-05-18 12:39:32 +0200

    feat(cloud): add insights settings UI

--- apps/cloud/app/routes/insights.tsx
+++ apps/cloud/app/routes/insights.tsx
@@ -2,9 +2,11 @@ import { Link, useLoaderData } from '@remix-run/react'
 import { getSongsCount } from '@repo/db/queries'
 import BoxMain from '@repo/ui/components/BoxMain'
 import Button from '@repo/ui/components/Button'
+import { cn } from '@repo/ui/helpers'
 import Body1 from '@repo/ui/typography/Body1'
 import Body2 from '@repo/ui/typography/Body2'
 import H3 from '@repo/ui/typography/H3'
+import Subtitle2 from '@repo/ui/typography/Subtitle2'
 import { json } from '@vercel/remix'
 import { ArrowLeft } from 'lucide-react'
 import { toNonBreaking } from 'node_modules/@repo/utils/dist/text/to-non-breaking'
@@ -18,6 +20,12 @@ export const loader = async () => {
 export default function Insights() {
     const { songsCount } = useLoaderData<typeof loader>()
 
+    const totalCount = songsCount.reduce((acc, { count }) => acc + count, 0)
+    const totalCallsToStage = songsCount.reduce(
+        (acc, { countStageCalls }) => acc + countStageCalls,
+        0,
+    )
+
     return (
         <BoxMain>
             <Button asChild className='float-start' variant='ghost' size='icon'>
@@ -28,18 +36,41 @@ export default function Insights() {
 
             <H3 className='min-h-9 px-10'>Insights</H3>
 
+            <div className='mt-10 grid grid-cols-3 gap-4'>
+                <div className='flex flex-col gap-2'>
+                    <Subtitle2>Filter</Subtitle2>
+                    <Button>Sessions</Button>
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <Subtitle2>Sort</Subtitle2>
+                    <Button>Total</Button>
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <Subtitle2>View</Subtitle2>
+                    <Button>Absolute</Button>
+                </div>
+            </div>
+
             <ul className='mt-8 space-y-2'>
-                <div className='flex justify-between gap-4'>
+                <div className='flex justify-between'>
                     <div />
-                    <div className='flex w-64 justify-between gap-2'>
-                        <Body2>Called to stage</Body2>
-                        <Body2>Total</Body2>
+                    <div className='flex w-80 justify-between gap-2 max-sm:w-full'>
+                        <Body2>Called to stage ({totalCallsToStage})</Body2>
+                        <Body2>Total ({totalCount})</Body2>
                     </div>
                 </div>
 
                 {songsCount.map(
                     ({ id, artist, title, count, countStageCalls }, index) => (
-                        <li key={id} className='flex justify-between gap-4'>
+                        <li
+                            key={id}
+                            className={cn(
+                                'flex justify-between gap-4 rounded-sm px-1 max-sm:flex-col',
+                                {
+                                    'bg-blue-900/50': index % 2 === 0,
+                                },
+                            )}
+                        >
                             <div>
                                 <Body2 className='inline'>
                                     {toNonBreaking(artist)}
@@ -49,14 +80,14 @@ export default function Insights() {
                                     {toNonBreaking(title)}
                                 </Body1>
                             </div>
-                            <div className='flex w-64 items-center justify-between gap-2'>
+                            <div className='flex w-80 shrink-0 items-center justify-between gap-2 max-sm:w-full'>
                                 <Body1 className='w-6 text-center'>
                                     {countStageCalls}
                                 </Body1>
                                 <div className='relative h-4 flex-grow overflow-hidden rounded-sm bg-white'>
                                     <div
                                         style={{
-                                            width: `${(countStageCalls / count) * 100}%`,
+                                            width: `${Math.round((countStageCalls / count) * 100)}%`,
                                         }}
                                         className='absolute left-0 top-0 h-full bg-blue-300'
                                     />

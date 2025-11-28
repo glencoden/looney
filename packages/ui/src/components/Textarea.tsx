8de3786d2e4080dd14563ee1a2a443731840e04e
Glen Coden <info@simonmeyer.de>
2025-11-28 12:14:13 +0100

    chore(ui): use tailwind 4 native class for field-sizing: content

--- packages/ui/src/components/Textarea.tsx
+++ packages/ui/src/components/Textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
         return (
             <textarea
                 className={cn(
-                    'field-content flex min-h-[42vh] w-full border-4 border-black bg-white px-3 py-2 text-base font-medium text-black ring-offset-0 drop-shadow file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:font-normal placeholder:italic placeholder:text-blue-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-pink-600 disabled:cursor-not-allowed disabled:text-blue-300',
+                    'field-sizing-content flex min-h-[42vh] w-full border-4 border-black bg-white px-3 py-2 text-base font-medium text-black ring-offset-0 drop-shadow file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:font-normal placeholder:italic placeholder:text-blue-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-pink-600 disabled:cursor-not-allowed disabled:text-blue-300',
                     className,
                 )}
                 ref={ref}

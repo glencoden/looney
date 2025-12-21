e673b8ce4c7dbe612c02abc76cc4bda7b437e9a6
Glen Coden <info@simonmeyer.de>
2025-12-21 18:08:31 +0100

    feat(website): get songs during build time from current db

--- apps/website/src/components/SongsPage.tsx
+++ apps/website/src/components/SongsPage.tsx
@@ -1,45 +1,33 @@
-import { useGet } from '../utils/useGet'
-import { LoadingSpinner } from './LoadingSpinner'
 import { VerticalSpacer } from './VerticalSpacer'
 
 interface Song {
     id: string
     artist: string
     title: string
+    genre: string | null
+    isFavorite: boolean
 }
 
-export function SongsPage() {
-    const { data, error, isLoading } = useGet(
-        'https://api.looneytunez.de/repertoire/published',
-    )
+interface SongsPageProps {
+    songs: Song[]
+}
 
+export function SongsPage({ songs }: SongsPageProps) {
     return (
         <>
             <VerticalSpacer />
 
-            {isLoading && <LoadingSpinner />}
-
-            {!isLoading && error && <pre>{JSON.stringify(error, null, 4)}</pre>}
-
-            {!isLoading && !error && !Array.isArray(data?.data?.songs) && (
-                <pre>{JSON.stringify(data, null, 2)}</pre>
-            )}
-
-            {!isLoading && !error && Array.isArray(data?.data?.songs) && (
-                <>
-                    {data.data.songs.map((song: Song) => {
-                        if (song.artist.length > 0 && song.title.length > 0) {
-                            return (
-                                <div key={song.id} className="song-box">
-                                    <p className="artist-name">{song.artist}</p>
-                                    <p className="song-title">{song.title}</p>
-                                </div>
-                            )
-                        }
-                        return null
-                    })}
-                </>
-            )}
+            {songs.map((song) => {
+                if (song.artist.length > 0 && song.title.length > 0) {
+                    return (
+                        <div key={song.id} className="song-box">
+                            <p className="artist-name">{song.artist}</p>
+                            <p className="song-title">{song.title}</p>
+                        </div>
+                    )
+                }
+                return null
+            })}
 
             <VerticalSpacer />
         </>
